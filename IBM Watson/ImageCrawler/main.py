97b30cfb6cf90c5a96a243846ffa9996aa8b8dc6
Khoa Nguyen 1 <knguy202@calpoly.edu>
2017-01-21 13:20:08 -0800

    remove calling statement

--- IBM Watson/ImageCrawler/main.py
+++ IBM Watson/ImageCrawler/main.py
@@ -1,7 +1,7 @@
 import requests
 import re
 from bs4 import BeautifulSoup
-def trade_spider(personsUserName, pathName):
+def getUrlsFromInsta(personsUserName, pathName):
     url = 'https://www.instagram.com/' + personsUserName + '/'
     sourceCode = requests.get(url)
     plain_text = sourceCode.text
@@ -25,5 +25,5 @@ def trade_spider(personsUserName, pathName):
             else:
                 break
 
-trade_spider('leopangchan', '/Users/khoanguyen1/Desktop/CPE/Side Projects/whoIsBetter/WhoIsBetter/IBM Watson/ImagesSetOne/ImagesetOne.txt')
+getUrlsFromInsta('leopangchan', '/Users/khoanguyen1/Desktop/CPE/Side Projects/whoIsBetter/WhoIsBetter/IBM Watson/ImagesSetOne/ImagesetOne.txt')
 
