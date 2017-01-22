1acccca765fb5975258c18cb5463755bc2790127
Zin Tun <ztun@Zins-MBP.dkn.io>
2017-01-21 17:59:43 -0800

    hashmap

--- IBM Watson/classifyImage.js
+++ IBM Watson/classifyImage.js
@@ -1,9 +1,16 @@
 //Authentication
 var watson = require('watson-developer-cloud');
 var fs = require('fs');
+var HashMap = require('hashmap');
+var async = require('async');
+var jsonfile = require('jsonfile')
+
+var map1 = new HashMap();
+var map2 = new HashMap();
+var asyncTasks = [];
 
 var visual_recognition = watson.visual_recognition({
-  api_key: '4bcaf7ff9e5b1edc09f79cd57f54516b7c36caee',
+  api_key: 'ad02145e5c374d4c8b8006527e43289966e67a64',
   version: 'v3',
   version_date: '2016-05-20'
 });
@@ -13,41 +20,109 @@ var params = {
   url : ""
 };
 // read the txt file line by line
-function readLines(input, func) {
+function readLines(input, map, func) {
   var remaining = '';
-
   input.on('data', function(data) {
     remaining += data;
     var index = remaining.indexOf('\n');
     while (index > -1) {
       var line = remaining.substring(0, index);
       remaining = remaining.substring(index + 1);
-      func(line);
+      func(line, map);
+
       index = remaining.indexOf('\n');
     }
   });
 
   input.on('end', function() {
     if (remaining.length > 0) {
-      func(remaining);
+      func(remaining, map);
     }
   });
 }
 
-function func(data) {
+function func(data, map) {
   console.log('Line: ' + data);
   params.url = data;
   visual_recognition.classify(params, function(err, res) {
     if (err)
       console.log(err);
-    else
-      console.log(JSON.stringify(res, null, 2));
+    else{
+      // var test = JSON.stringify(res, null, 2);
+      // console.log(test);
+      var obj = res.images[0].classifiers[0].classes;
+      var value = 1;
+      for(var i = 0; i < obj.length; i++) {
+        var key = JSON.stringify(obj[i].class, null, 2);
+        var value = JSON.stringify(obj[i].score, null, 2);
+        if(map.get(key) >= 0)
+          value = parseInt(value) + parseInt(map.get(key));
+        console.log(key + " " + value);
+        // map.get(key);
+        map.set(key, value);
+      }
+    }
   });
-
 }
 
-var input = fs.createReadStream('./ImagesSetOne/ImagesetOne.txt');
-readLines(input, func);
+
+
+async.series([
+  function(callback){
+    var input1 = fs.createReadStream('./ImagesSetOne/ImagesetOne.txt');
+    readLines(input1, map1, func);
+    callback(null);
+  },
+  function(callback){
+    console.log("MEEEEEEEEEE");
+    // readLines(input1, map1, func);
+    var file1 = './ImagesSetOne/data1.json'
+    // var json1 = [], item;
+    // for (var type in map1) {
+    //     if (map1.hasOwnProperty(type)) {
+    //         item = {};
+    //         item.type = type;
+    //         item.name = map1[type];
+    //         json1.push(item);
+    //     }
+    // }
+    jsonfile.writeFile(file1, map1, {spaces: 2}, function(err) {
+      console.error(err);
+    });
+    callback(null);
+  },
+  function(callback){
+    var input2 = fs.createReadStream('./ImagesSetTwo/ImagesetTwo.txt');
+    readLines(input2, map2, func);
+    callback(null);
+  },
+  function(callback){
+    // readLines(input1, map1, func);
+    var file2 = './ImagesSetTwo/data2.json'
+    // var json1 = [], item;
+    // for (var type in map1) {
+    //     if (map1.hasOwnProperty(type)) {
+    //         item = {};
+    //         item.type = type;
+    //         item.name = map1[type];
+    //         json1.push(item);
+    //     }
+    // }
+    jsonfile.writeFile(file2, map2, {spaces: 2}, function(err) {
+      console.error(err);
+    });
+    callback(null);
+  }
+]);
+
+// map1.forEach(function(value, key) {
+//     console.log(key + " : " + value);
+// });
+// var json2 = JSON.stringify(map2);
+// var file2 = './ImagesSetTwo/data2.json'
+// jsonfile.writeFile(file2, json2)
+
+
 //
 
 //
